8b2f95e858ebded8e1ec69d4b8c078e94daea38d
Elliot Nelson <nelson.work@gmail.com>
2021-09-29 09:30:55 -0400

    Rename phase names to be more realistic

--- libraries/acme-utils/scripts/build.js
+++ libraries/acme-utils/scripts/build.js
@@ -1,12 +1,19 @@
 'use strict';
 
 // Because the project's build got more complicated with multiple steps, a home-grown
-// build script was invented so they could pass along "--lite" to their individual scripts.
+// build script was invented.
+//
+// This script is the equivalent of a "build" script like:
+//
+//   heft test && node scripts/update-readme.js
 
 const { execSync } = require('child_process');
 
 const lite = process.argv.includes('--lite');
 const heftCommand = lite ? 'heft build --lite --clean' : 'heft test --clean';
 
+// heft build
 execSync(heftCommand, { stdio: 'inherit', encoding: 'utf8' });
-execSync(`node ${__dirname}/readme_updater.js`, { stdio: 'inherit', encoding: 'utf8' });
+
+// update-readme
+execSync(`node ${__dirname}/update-readme.js`, { stdio: 'inherit', encoding: 'utf8' });
